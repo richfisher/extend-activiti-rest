2292fa085a6183a3e2f0505ea9b51082b33020ed
richfisher <richfisher.pan@gmail.com>
2015-02-26 17:49:54 +0800

    First commit.Using spring boot. Can get /greeting and return "Hello World!"

---
+++ src/main/java/fisher/extend_activiti_rest/Application.java
@@ -0,0 +1,11 @@
+package fisher.extend_activiti_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
