d8d3a4646076c8cd12736587712ee0bf7a74bf76
richfisher <richfisher.pan@gmail.com>
2015-02-27 10:49:05 +0800

    Making it running! But it can not load db.properties properly.

--- src/main/java/fisher/extend_activiti_rest/Application.java
+++ src/main/java/fisher/extend_activiti_rest/Application.java
@@ -1,11 +1,15 @@
 package fisher.extend_activiti_rest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
 }
