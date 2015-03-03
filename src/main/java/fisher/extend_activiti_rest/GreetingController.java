1328d6d009ffc8b7ee39943fd71a3b37652e5e2e
richfisher <richfisher.pan@gmail.com>
2015-03-03 15:22:06 +0800

    fix db properties. And load processEngine in GreetingController.

--- src/main/java/fisher/extend_activiti_rest/GreetingController.java
+++ src/main/java/fisher/extend_activiti_rest/GreetingController.java
@@ -4,10 +4,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+
+import static java.lang.System.*;
+import java.util.*;
+
 @RestController
 public class GreetingController {
+    @Autowired
+    private ApplicationContext appContext;
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        ProcessEngine processEngine = appContext.getBean(ProcessEngine.class);
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> availableTaskList = taskService.createTaskQuery().list();
+        System.out.println(availableTaskList.size());
+
         return "Hello World!";
     }
 }
